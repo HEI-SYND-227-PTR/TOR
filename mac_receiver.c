b9a6a74bd002663c98f0f1ca533ac48baa085e41
ChaiseM <maxime.cesalli@students.hevs.ch>
2023-04-18 11:46:00 +0200

    Token working with Updated List and LCD

--- mac_receiver.c
+++ mac_receiver.c
@@ -8,13 +8,34 @@
 void MacReceiver(void *argument)
 {
 	struct queueMsg_t message;
+	osStatus_t returnPHY;
+	struct queueMsg_t myMessage = {0};
 	
 	while(1)
 	{
 		osMessageQueueGet(queue_macR_id,&message,NULL,osWaitForever);
 		if(message.type == FROM_PHY)
 		{
-			printf("WAHAAAAAA\r\n");
+			uint8_t* dataptr;
+			dataptr = message.anyPtr;
+			
+			if (dataptr[0] == TOKEN_TAG)
+			{
+				uint8_t* tkDataBack = osMemoryPoolAlloc(memPool,osWaitForever);
+				printf("A TOKEN WAS RECIVED \r\n");
+				for(int i = 1; i <= 16 ; i++)
+				{
+					tkDataBack[i-1] = dataptr[i];
+				}
+				
+					// creation of a message
+					myMessage.type = TOKEN;
+					myMessage.anyPtr = tkDataBack;			
+				
+					
+					returnPHY = osMessageQueuePut(queue_macS_id,&myMessage,NULL,osWaitForever);
+					CheckRetCode(returnPHY,__LINE__,__FILE__,CONTINUE);
+			}
 		}
 		
 	}
--- mac_sender.c
+++ mac_sender.c
@@ -7,33 +7,77 @@
 
 void MacSender(void *argument)
 {
-	//TODO
+	
 	struct queueMsg_t message;
-	struct queueMsg_t myPhyMessage;
+	struct queueMsg_t myMessage = {0};
 	osStatus_t returnPHY;
 	//Read The macR queue
 	while(1)
 	{
+		
+		// getting the messages from the queue
 		osMessageQueueGet(queue_macS_id,&message,NULL,osWaitForever);
+		// if the message is of type TOKEN
+		if(message.type == TOKEN)
+		{		
+				uint8_t* dataptr;
+				dataptr = message.anyPtr;
+		
+				// we need to update the list of connected stations 
+				for(int i = 0; i <= 15 ; i++)
+				{
+					gTokenInterface.station_list[i] = dataptr[i];
+					printf("station %d data : 0x%02x \r\n", i+1, dataptr[i]);
+				}
+				
+					// creation of a message
+					myMessage.type = TOKEN_LIST;
+					
+					//updating the que of the LCD
+					returnPHY = osMessageQueuePut(queue_lcd_id,&myMessage,NULL,osWaitForever);
+					CheckRetCode(returnPHY,__LINE__,__FILE__,CONTINUE);
+		}
+		if(message.type == START)
+		{
+			gTokenInterface.connected = true;
+		}
+		// if the message is of type STOP
+		if(message.type == STOP)
+		{
+			gTokenInterface.connected = false;
+		}
+		// if the message is of type NEW_TOKEN
 		if(message.type == NEW_TOKEN)
 		{
 			printf("WOHOOOW\r\n");
 			// creation of the Token
 			uint8_t* ptk = osMemoryPoolAlloc(memPool,osWaitForever);
-			// TODO verifier que ptk != NULL
-			memset(ptk,0,17);
-			ptk[0] = 0xff;
-			ptk[MYADDRESS] = 0x2A;
+			if (ptk)
+			{
+					// putting zeros everywhere
+					memset(ptk,0,17);
+					// token header
+					ptk[0] = TOKEN_TAG;
+					// putting the correct value inside the token
+					//	time sapi is alwais active	
+					ptk[MYADDRESS] = (1 << TIME_SAPI);
+					// if we are connected
+					if(gTokenInterface.connected)
+					{
+						// chat sapi must be active
+						ptk[MYADDRESS] |= (1 << CHAT_SAPI) ;
+					}
+					
+				
+					// creation of a message
+					myMessage.type = TO_PHY;
+					myMessage.anyPtr = ptk;			
 		
-			
-			myPhyMessage.type = TO_PHY;
-			myPhyMessage.anyPtr = ptk;			
-			myPhyMessage.sapi = NULL;
-			myPhyMessage.addr = NULL;
-			
-			
-			returnPHY = osMessageQueuePut(queue_phyS_id,&myPhyMessage,NULL,osWaitForever);
-			CheckRetCode(returnPHY,__LINE__,__FILE__,CONTINUE);
+					
+					returnPHY = osMessageQueuePut(queue_phyS_id,&myMessage,NULL,osWaitForever);
+					CheckRetCode(returnPHY,__LINE__,__FILE__,CONTINUE);
+			}
+		
 		}
 	}
 	
